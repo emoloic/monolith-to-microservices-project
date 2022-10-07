0fdfc9c6d46e42e515afae74b81e00ff4966058f
emo <emoloic56@gmail.com>
2022-10-07 21:02:26 +0100

    first commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a153947b5bddb491680fac9f24561645-2022811654.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
